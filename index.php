8fa0e8b3b9c9c40e0d0629d67f8f00e841ac2640
AliNAU2020 <31328423+AliNAU2020@users.noreply.github.com>
2020-08-31 21:33:49 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,4 @@
 <?php header ('Location: /index.html') ; ?>
+<?php header ('Location: /register_account.html') ; ?>
+<?php header ('Location: /luncheons.html') ; ?>
+<?php header ('Location: /tickets.html') ; ?>
